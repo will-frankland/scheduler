a32158f9855c15244fb7aa68cc82bf9b7086930b
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-04-20 18:08:52 -0400

    Add show component

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,8 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/header"
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 
 storiesOf("Button", module)
@@ -142,3 +144,11 @@ storiesOf("InterviewerList", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => <Show
+    student={"Lydia Miller-Jones"}
+    interviewer={interviewer}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+    />)
+    
